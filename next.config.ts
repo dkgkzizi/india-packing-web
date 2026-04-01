f39f9c5efae083cc8cd4765cc5e975a3fb050e00
dkgkzizi <gktehrm114@gmail.com>
2026-04-01 16:18:25 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 빌드 시 타입 에러와 린트 에러를 무시하도록 설정합니다.
+  typescript: {
+    ignoreBuildErrors: true, 
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
